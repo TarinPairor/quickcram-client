aa74b030af4a82c7713d402e5fd91a4823a5cc44
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-12-22 00:00:15 +0800

    Test out blank test page

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/test" element={<Test />} />
+            <Route path="/test" element={<>Test</>} />
             <Route path="/auth/callback" element={<HandleAuthCallback />} />
           </Routes>
         </Router>
