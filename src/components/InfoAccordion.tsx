2e91fedf2a3dc2199e9ee37083c9fa0cce1323bf
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-12-25 00:27:52 +0800

    Refactor InfoAccordion items

--- src/components/InfoAccordion.tsx
+++ src/components/InfoAccordion.tsx
@@ -5,6 +5,57 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+const subElements = [
+  {
+    question: "What does QuickCram do?",
+    answer:
+      "Type (or record) in an event description, and the AI will generate a calendar event for you in Google Calendar!",
+  },
+  {
+    question: "How can I write my event? Give an example.",
+    answer: "Meet John at 2pm on Friday at Starbucks.",
+  },
+  {
+    question: "What details should I provide?",
+    answer:
+      "At least a summary. A start time, and an end or time period should be provided.",
+  },
+  {
+    question: "Any extra details?",
+    answer: "A location and a description are optional, but can be helpful.",
+  },
+  {
+    question: "What is the Start/Stop button?",
+    answer:
+      "It can record your voice and convert it to text. FYI, it resets when you click it again.",
+  },
+  {
+    question:
+      "Do I have to set up anything for it to connect to my Google Calendar?",
+    answer: "Just login and you're good to go.",
+  },
+  {
+    question: "Why can't I see the event on my Google Calendar after adding?",
+    answer:
+      "If there is an access error, try getting access again by clicking on the Profile Picture - Get Access Token.",
+  },
+];
+
+const SubElement = ({
+  value,
+  question,
+  answer,
+}: {
+  value: string;
+  question: string;
+  answer: string;
+}) => (
+  <AccordionItem value={value} className="w-full">
+    <AccordionTrigger>{question}</AccordionTrigger>
+    <AccordionContent>{answer}</AccordionContent>
+  </AccordionItem>
+);
+
 export default function InfoAccordion() {
   return (
     <Accordion type="multiple" className="w-full">
@@ -16,45 +67,16 @@ export default function InfoAccordion() {
           <Accordion
             type="single"
             collapsible
-            className=" flex flex-col items-center"
+            className="flex flex-col items-center"
           >
-            <AccordionItem value="item-1" className="w-full">
-              <AccordionTrigger>How does it work?</AccordionTrigger>
-              <AccordionContent className="">
-                Type in a general event description, and the AI will generate a
-                calendar event for you!
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2" className="w-full">
-              <AccordionTrigger>
-                Do I have to set up anything for it to connect to my Google
-                Calendar
-              </AccordionTrigger>
-              <AccordionContent className="">
-                Nope. Just login and you're good to go.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3" className="w-full">
-              <AccordionTrigger>What is an example prompt?</AccordionTrigger>
-              <AccordionContent className="">
-                Meet John at 2pm on Friday at Starbucks.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-4" className="w-full">
-              <AccordionTrigger>
-                What details should I provide?
-              </AccordionTrigger>
-              <AccordionContent className="">
-                At least a summary. A start time, and an end time or length
-                should be provided.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-5" className="w-full">
-              <AccordionTrigger>Any extra details?</AccordionTrigger>
-              <AccordionContent className="">
-                A location and a description are optional, but can be helpful.
-              </AccordionContent>
-            </AccordionItem>
+            {subElements.map((element, index) => (
+              <SubElement
+                key={"item-" + index}
+                value={"item-" + index}
+                question={element.question}
+                answer={element.answer}
+              />
+            ))}
           </Accordion>
         </AccordionContent>
       </AccordionItem>
