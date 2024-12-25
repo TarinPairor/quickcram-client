82d0bb7b0ee9ebfb2b5047224f59e4fd86b71087
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-12-25 23:55:12 +0800

    Remove GoogleLoginButton unsused imports

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,18 +1,10 @@
 import { fetchUserPayload } from "@/functions/fetchUserPayload";
 import { redirectToAuthPage } from "@/functions/redirectToAuthPage";
-// import googleLogo from "../assets/google-logo.svg";
 import { GoogleLogin } from "@react-oauth/google";
 
 const GoogleLoginButton = () => {
   return (
     <>
-      {/* <button
-        onClick={redirectToAuthPage}
-        className="flex items-center justify-center px-4 py-2 bg-black border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-      >
-        <img src={googleLogo} alt="Google Logo" className="w-6 h-6 mr-2" />
-        <span className="text-gray-700 font-medium">Sign in with Google</span>
-      </button> */}
       <GoogleLogin
         size="large"
         shape="rectangular"
